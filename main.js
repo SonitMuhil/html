e301149c07e6ed606d1c54bdebda104ecc3903da
SonitMuhil <77870888+SonitMuhil@users.noreply.github.com>
2021-06-07 16:43:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-var images=[
-    "family.jpg","father.jpg","moher.jpg","uncle.jpg","aunt.png"
-];
-var names=[
-    "Family Book","Praveen Bhaskaran","Sasimaladevi","Karthik Kumar","Shruti"];
-var i=0;
-function update(){
-    i++;
-    var number_of_family_membes_in_array = 5
-    if(i>number_of_family_membes_in_array)
-{
-i = 0;
+menu_list_array = ["Veg Margherita Pizza",
+"Chicken Tandoori Pizza",
+"Veg Supreme Pizza",
+"Paneer Tikka Pizza",
+"Deluxe Veggie",
+"Veg Extravaganza Pizza"];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>'+menu_list_array[i]+'</li>'
 }
-var updatedImage=images[i];
-var updatedName=names[i];
-document.getElementById("familymi").src=updatedImage;
-document.getElementById("familymn").src=updatedName;
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML=htmldata;
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata=htmldata+"<section class='cards'>"
+for(var i=0; i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="card">'
++'<img src="images/pizzaImg.png"/>'
++menu_list_array[i]+'</div>'
 }
+htmldata=htmldata+"</section>"
+document.getElementById("display_addedmenu").innerhtml=htmldata;
+
+function add_top(){
+var item=document.getElementById("add_item").value;
+menu_list_array.push(item);
+add_item();
+}
+menu_list_array.sort();
+var item=document.getElementById("add_item").value;
+menu_list_array.push(item);
